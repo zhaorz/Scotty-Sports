90de72ce6e909613199fdb71f9190c377bb8efc9
Eric Lee <eric.lee2579@gmail.com>
2015-02-07 00:35:43 -0500

    page 2

--- ScottySports/ViewController.swift
+++ ScottySports/ViewController.swift
@@ -14,7 +14,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+
+@IBAction func Running(sender: AnyObject) {
+}
+@IBAction func Tennis(sender: AnyObject) {
+}
+@IBAction func Baseball(sender: AnyObject) {
+}
+@IBAction func Squash(sender: AnyObject) {
+    var object1 = PFObject(className: "users")
+    object1.addObject("Squash", forKey: "ayy")
+    object1.saveInBackgroundWithTarget(nil , selector: nil)
+}
+@IBAction func Frisbee(sender: AnyObject) {
+    var object1 = PFObject(className: "users")
+    object1.addObject("Frisbee", forKey: "ayy")
+    object1.saveInBackgroundWithTarget(nil , selector: nil)
+}
+@IBAction func Basketball(sender: AnyObject) {
+    var object1 = PFObject(className: "users")
+    object1.addObject("Basketball", forKey: "ayy")
+    object1.saveInBackgroundWithTarget(nil , selector: nil)
+}
+@IBAction func Football(sender: AnyObject) {
+var object1 = PFObject(className: "users")
+    object1.addObject("Football", forKey: "ayy")
+    object1.saveInBackgroundWithTarget(nil , selector: nil)
+}
     
 @IBAction func switcher(sender: AnyObject) {
     var object1 = PFObject(className: "users")
@@ -22,12 +48,9 @@ class ViewController: UIViewController {
     object1.saveInBackgroundWithTarget(nil , selector: nil)
 
 }
-    @IBAction func Soccer(sender: AnyObject) {
-        var object1 = PFObject(className: "users")
-        object1.addObject("usernameHere", forKey: "Sports")
-    object1.saveInBackgroundWithTarget(nil , selector: nil)
-        
-    }
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
